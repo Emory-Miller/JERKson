77af91bd13e4ada5e363700995ebfd61bc1a7e97
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-12-12 13:58:34 -0500

    Finished ItemParser Class... was not feeling well this morning, did not get start until later in the day

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,32 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ItemParser {
-    public List<Item> parseItemList(String valueToParse) {
-        return null;
+    public List<Item> parseItemList(String valueToParse) throws ItemParseException {
+        String lowerCase = valueToParse.toLowerCase();
+        String[] delimString = lowerCase.split("##");
+        List<Item> itemList = new ArrayList<>();
+        try {
+            for (String str : delimString) {
+                Item item = parseSingleItem(str);
+                itemList.add(item);
+            }
+        } catch ( ItemParseException e){
+            System.out.println("Error");
+        }
+        return itemList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        String lowerCase = singleItem.toLowerCase();
+        String[] delimString = lowerCase.split("[:;@\\^%#]");
+
+        Item item = new Item(delimString[1], Double.parseDouble(delimString[3]), delimString[5], delimString[7]);
+
+        return item;
     }
 }
